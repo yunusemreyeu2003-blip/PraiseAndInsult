6021a133658e31fdeb0a5ae105355ceb26eb3d7a
Övünç Aygün <ovunc.aygun@gmail.com>
2026-02-01 16:15:13 +0300

    Added State enumeration for test pages

---
+++ Interface/State.java
@@ -0,0 +1,9 @@
+package Interface;
+
+public enum State {
+    COUNTDOWN,
+    FINISHED,
+    WAIT,                   // White
+    TRIGGER,                // Red
+    FALSE_TRIGGER           // Blue
+}
