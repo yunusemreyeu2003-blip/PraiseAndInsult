73bb88040453a6139276e6d1b594d1b64979677d
Övünç Aygün <ovunc.aygun@gmail.com>
2026-02-02 17:24:16 +0300

    Added DurationHelper which converts duration variable types

---
+++ Time/DurationHelper.java
@@ -0,0 +1,34 @@
+package Time;
+
+public final class DurationHelper {
+
+    private DurationHelper() {}
+
+    public static java.time.Duration DoubleSecondsToJava(double seconds) {
+        long nanos = (long) (seconds * 1_000_000_000);
+        return java.time.Duration.ofNanos(nanos);
+    }
+
+    public static javafx.util.Duration DoubleSecondsToJavaFX(double seconds) {
+        double millis = seconds * 1000.0;
+        return javafx.util.Duration.millis(millis);
+    }
+
+    public static double JavaToDoubleSeconds(java.time.Duration duration) {
+        return duration.toNanos() / 1_000_000_000.0;
+    }
+
+    public static double JavaFXToDoubleSeconds(javafx.util.Duration fxDuration) {
+        return fxDuration.toMillis() / 1000.0;
+    }
+
+    public static javafx.util.Duration JavaToJavaFX(java.time.Duration duration) {
+        double millis = duration.toNanos() / 1_000_000.0;
+        return javafx.util.Duration.millis(millis);
+    }
+
+    public static java.time.Duration JavaFXToJava(javafx.util.Duration fxDuration) {
+        long nanos = (long) (fxDuration.toMillis() * 1_000_000);
+        return java.time.Duration.ofNanos(nanos);
+    }
+}
