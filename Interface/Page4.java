a9aec7f7124d6bc062de08cf297099208083f824
Övünç Aygün <ovunc.aygun@gmail.com>
2026-02-02 17:27:14 +0300

    Added Page4

---
+++ Interface/Page4.java
@@ -0,0 +1,36 @@
+package Interface;
+
+import Data.*;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+
+public class Page4 extends Page{
+    Text text;
+
+    public Page4(Data data, Scene scene, StackPane root) {
+        super(data, scene, root);
+
+        upperStack.prefHeightProperty().bind(scene.heightProperty().multiply(0.2));
+        bottomStack.prefHeightProperty().bind(scene.heightProperty().multiply(0.1));
+        leftStack.prefWidthProperty().bind(scene.widthProperty().multiply(0.2));
+        rightStack.prefWidthProperty().bind(scene.widthProperty().multiply(0.2));
+
+        navigation.setAlignment(Pos.CENTER);
+        navigation.spacingProperty().bind(bottomStack.widthProperty().multiply(0.1));
+
+        prevButton.setText("Previous");
+        nextButton.setText("Next");
+        navigation.getChildren().addAll(prevButton, nextButton);
+
+        centerStack.setAlignment(Pos.CENTER);
+
+        text = new Text("Sample Text");
+        text.wrappingWidthProperty().bind(scene.widthProperty().multiply(0.6));
+        text.setTextAlignment(TextAlignment.CENTER);
+        centerStack.getChildren().addAll(text);
+    }
+}
